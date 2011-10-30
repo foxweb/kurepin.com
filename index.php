8975ee4ee543a2bbf4e4fb5bf1a000fe709858e2
Aleksey Kurepin <a.kurepin@gmail.com>
2011-10-30 23:18:18 +0400

    вот тут все мои файлы :)

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Алексей Курепин, веб-мастер</title>
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link href="/css/resume.css" rel="stylesheet" type="text/css" media="all" />
+<meta content="Алексей Курепин" name="author" />
+</head>
+<body>
+<h1>Алексей Курепин <sup>сейчас работу не ищу</sup></h1>
+<div><img src="/img/resume.png" alt="Алексей Курепин" id="image" height="120" width="120" />
+<div id="about">Веб-мастер, эксперт по веб-технологиям, разработчик веб-приложений, сайтов и сервисов.<br />Выпускник Пензенского государственного университета, 2007 г.<br 
+/>Диплом инженера по специальности &laquo;Программное обеспечение вычислительной техники и автоматизированных систем&raquo;.<br /><br />25 лет, г. Москва<br />
+<strong>Почта:</strong> <a href="mailto:lesha@kurepin.com">lesha@kurepin.com</a><br />
+<strong>Skype:</strong> foxweb_fox</div>
+</div>
+<h1>Опыт работы</h1>
+<div>
+<ul>
+  <li><strong>Июль 2010 г.</strong> &mdash; руководитель и разработчик веб-проектов круизной компании &laquo;<a href="http://breeze.ru">Бризлайн</a>&raquo;. Отвечаю за 
+интернет-магазин круизов и его интеграцию с международными системами бронирования, а также платёжными системами и 1С.</li>
+  <li><strong>Ноябрь 2009 г.</strong> &mdash; ведущий веб-разработчик в книжном интернет-магазине <a href="http://read.ru">Read.ru</a>.</li>
+  <li><strong>Январь 2009 г.</strong> &mdash; работа в &laquo;<a href="http://alt1.ru/">Первом альтернативном контент-провайдере</a>&raquo;, где занимался разработкой серверной части мобильных сервисов (информационно-развлекательных) на платформах SMS, USSD, STK/DSTK, WAP.</li>
+  <li><strong>Октябрь 2007 г.</strong> &mdash; поступление на работу в медиахолдинг &laquo;Пронто-Москва&raquo;. Участвовал в разработке таких известных ресурсов, как каталог объявлений &laquo;<a href="http://irr.ru">Из рук в руки</a>&raquo;, <a href="http://job.ru">Job.ru</a>, полностью с нуля разработан сайт <a href="http://domania.ru">Domania.RU</a>.</li>
+  <li><strong>Декабрь 2006 г.</strong> &mdash; сдача в эксплуатацию <a href="http://penza-gorod.ru">сайта Пензенской городской администрации</a>. Применены: многопользовательская система управления с разграничением прав доступа, древовидная структура разделов, страничная система (возможность неограниченного создания страниц), полное управление всеми разделами, визуальный редактор текстов наподобие Word с возможностью вставки картинок и закачки документов. Применена семантическая вёрстка по стандартам W3C. Сайт доступен для мобильных устройств и оптимизирован для печати. В настоящее время не поддерживается.</li>
+  <li><strong>Май 2006 г.</strong> &mdash; начата работа над <a href="http://penza-gorod.ru">сайтом Пензенской городской администрации</a>.</li>
+  <li><strong>Апрель 2006 г.</strong> &mdash; участвовал в создании стартапа &laquo;<a href="http://ir-design.ru/">Интернет-Решение</a>&raquo; (веб-студия) в должности ведущего веб-мастера. Сдал в работу несколько коммерческих сайтов.</li>
+  <li><strong>Февраль 2004&mdash;2005 г.</strong> &mdash; разработка и поддержка <a href="http://58region.ru/">регионального информационного портала 58region.ru</a>. Цель работы &mdash; &laquo;с нуля&raquo; разработать динамическую управляемую версию сайта. Первая версия &mdash; на языке Perl, вторая версия &mdash; на основе шаблонно-модульной системы. Проект закрыт.</li>
+</ul>
+</div>
+<h1>Специализация</h1>
+<div>
+<ul>
+  <li><strong>HTML</strong>, <strong>XML</strong>, <strong>XHTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>jQuery</strong>, <strong>AJAX</strong>, <strong>RSS</strong>, семантическая/блочная вёрстка, единая вёрстка для ПК, мобильных и печатающих устройств, полная поддержка стандартов и рекомендаций <strong>W3C</strong>.</li>
+  <li><strong>Perl</strong>, <strong>PHP 5</strong>, <strong>MySQL 5</strong>, <strong>Apache 2</strong> &mdash; умение программировать, включая установку, настройку, администрирование.</li>
+  <li><strong>Nginx</strong> &mdash; конфигурирование, начальный уровень. Есть опыт интеграции с Rails-приложениями (например, Redmine).</li>
+  <li><strong>Ruby</strong>, <strong>Ruby on Rails</strong> &mdash; средний уровень. Новое &mdash; под Ruby&nbsp;1.9 + Rails&nbsp;3.0, старое &mdash; под Ruby&nbsp;1.8.6 + <nobr>Rails&nbsp;2.0-2.3</nobr>; также умею <strong>Passenger</strong>, <strong>Mongrel</strong>, кластеризация, memcache.</li>
+  <li>Создание и поддержка сайтов на <strong>Drupal</strong>, а так же создание систем управления по индивидуальным требованиям.</li>
+  <li>Рабочие инструменты: <strong>Ubuntu</strong>, <strong>Aptana</strong>, <strong>Eclipse</strong>, <strong>NetBeans</strong>, <strong>SVN</strong>, <strong>Git</strong>, <strong>jira</strong>, <strong>TestTracker</strong>, <strong>Мотив</strong>.</li>
+  <li>Администрирование, установка серверов, настройка операционных систем и прикладного программного обеспечения в среде Linux.</li>
+  <li>Базы данных <strong>MySQL</strong> &mdash; разработка любого программного обеспечения под <strong>MySQL</strong> в средах веб, Windows, Linux, грамотная разработка архитектуры БД, репликация, резервирование, администрирование сервера <strong>MySQL</strong>.</li>
+  <li>Разработка консольных приложений под Linux на <strong>Perl</strong>, <strong>PHP</strong>, <strong>C/C++</strong> (начальный уровень).</li>
+  <li>Интеграция <strong>веб-сервисов</strong>, <strong>WebMoney</strong>, <strong>SMS-сервисы</strong>.</li>
+  <li>Веб-приложения на основе <strong>AJAX</strong>, <strong>Zend Framework</strong>, <strong>jQuery</strong>, <strong>extJS</strong>. Готов к работе с другими веб-фреймворками.</li>
+  <li>Знание принципов работы и оптимизации высоконагруженных веб-проектов.</li>
+  <li>Руководство веб-проектами, координация работы дизайнеров и программистов, написание проектной документации, консультации.</li>
+  <li>Поддержка свободного ПО, Linux, открытых систем и стандартов (в частности, рекомендаций W3C и открытых сетевых протоколов), Web 2.0.</li>
+  <li>Всё остальное могу легко <a href="http://lmgtfy.com/?q=%D1%8F+%D1%83%D0%BC%D0%B5%D1%8E+%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F+%D0%B3%D1%83%D0%B3%D0%BB%D0%BE%D0%BC">нагуглить</a>.</li>
+</ul>
+<p>Не работаю с такими технологиями:</p>
+<ul>
+  <li>Все средства разработки <strong>Sun Microsystems</strong>.</li>
+  <li>Все средства разработки <strong>Microsoft</strong>.</li>
+  <li>Все средства разработки <strong>Adobe</strong>.</li>
+</ul>
+<p>Просьба не предлагать:</p>
+<ul>
+  <li>Нарисовать кнопочки.</li>
+  <li>Сверстать страничку.</li>
+  <li>Сделать, чтоб Яндекс/Гугл сайт находил.</li>
+</ul>
+<p>В будущем мне хотелось бы изучить:</p>
+<ul>
+  <li>Python.</li>
+</ul>
+</div>
+<h1>Другие сведения</h1>
+<div>
+    <strong>Семейное положение:</strong> не женат, детей нет.<br />
+    <strong>Водительский опыт:</strong> категория &laquo;B&raquo; с  2005 г.<br />
+    <strong>Личный автомобиль:</strong> нет.<br />
+    <strong>Знание языков:</strong> отличное знание русского (пишу грамотно и без ошибок), технический английский (программирование), начальные знания французского. <br />
+    <strong>Психология:</strong> уравновешенный и серьёзный, но с хорошим чувством юмора, в меру общительный и доброжелательный, хорошо уживаюсь в коллективе. При определённых условиях проявляю разумную инициативу и лидерские качества.<br />
+    <strong>Характер в работе:</strong> склонен к разумной оптимизации всего, не тороплюсь, стараюсь сделать всё так, чтобы потом не переделывать.<br />
+    <strong>Хобби:</strong> музыка, фотография, интернет, электроника, путешествия.<br />
+    <strong>Готовность к работе:</strong> до 2 недель со дня принятия решения об устройстве на работу.<br />
+    <strong>Командировки:</strong> непродолжительные по РФ.
+</div>
+<div id="counter">Последнее обновление от 13 сентября 2010 года.<br />
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-5711316-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
