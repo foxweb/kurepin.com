8169eb37c4945dfd040f7c3e653375e3f4807aef
Aleksey Kurepin <lesha@kurepin.com>
2023-01-14 23:39:21 +0300

    mina deploy scrip updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,52 @@
-require 'mina/git'
+# frozen_string_literal: true
 
-set :domain, '37.139.12.234'
-set :user, 'foxweb'
+require 'mina/git'
+require 'mina/rbenv'
+require 'mina/deploy'
+
+set :application_name, 'kurepin.com'
+set :domain, 'kurepin.com'
 set :deploy_to, '/home/foxweb/www/kurepin.com'
 set :repository, 'git@github.com:foxweb/kurepin.com.git'
 set :branch, 'master'
 set :shared_paths, %w[public/pub public/photo public/vk public/.well-known]
-set :keep_releases, 5
+set :user, 'foxweb'
 
-task setup: :environment do
-  queue! %(mkdir -p "#{deploy_to}/shared/public/pub")
-  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/public/pub")
+# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
+# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
+set :shared_dirs, fetch(:shared_dirs, []).push(*%w[public/pub public/photo public/vk public/.well-known])
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
-  queue! %(mkdir -p "#{deploy_to}/shared/public/photo")
-  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/public/photo")
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :remote_environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
 
-  queue! %(mkdir -p "#{deploy_to}/shared/public/vk")
-  queue! %(chmod g+rx,u+rwx "#{deploy_to}/shared/public/vk")
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use', 'ruby-2.5.3@default'
+end
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup do
+  # command %{rbenv install 2.5.3 --skip-existing}
+  # command %{rvm install ruby-2.5.3}
+  # command %{gem install bundler}
 end
 
 desc 'Deploys the current version to the server.'
-task deploy: :environment do
+task :deploy do
+  invoke :'git:ensure_pushed'
+
   deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'deploy:cleanup'
 
-    to :launch do
+    on :launch do
       # nothing
     end
   end
